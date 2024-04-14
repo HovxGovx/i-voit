287e50d71d67fab100a61c2aea78b90dbba7eaa9
HovxGovx <angelotrandria69@gmail.com>
2024-04-14 11:05:46 +0300

    nav bar avec button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
+  
   return (
     <div className="App">
       <Navbar/>
+      <LoginSignUp/>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,74 +1,110 @@
-import React, { Component } from 'react'
-import "./NavbarStyles.css"
-import logo from '../Assets/Icons/covoiturage.png'
-import voyage from '../Assets/Icons/covoiturage(1).png'
-import ajout from '../Assets/Icons/utilisateur-du-cercle.png'
-class Navbar extends Component{
-    state = {clicked: false }
-    HandleClick  = () =>{
-        this.setState({
-            clicked: !this.state.clicked
-        })
-    }
-    render(){
-  return (
-    <div>
-      <nav id='trois'>
-        <a href="#trois">
-            <img src={logo} 
-            id='logo-15'
-            width='59'
-            height='58'
-            viewBox="0 0 59 58"
-            fill="none"
-            alt='noalt'/>
-        </a>
-            <div>
-                <ul id="navbar">
-                    <li>
-                        <a href="#trois" className="active">
-                            Trajet</a>
-                        </li>
-                    <li>
-                        <a href="#trois" className="trois">
-                            Partager</a>
+import React, { useState } from 'react';
+import "./NavbarStyles.css";
+import logo from '../Assets/Icons/covoiturage.png';
+import ajout from '../Assets/Icons/utilisateur-du-cercle.png';
+
+const Navbar = ({ onOptionChange }) => {
+    const [activeButton, setActiveButton] = useState('trajet');
+
+    const handleClick = (option) => {
+            
+            setActiveButton(option);
+    };
+    
+
+    return (
+        <div>
+            <nav id='navbar'>
+                <img
+                    src={logo}
+                    id='logo-15'
+                    width='59'
+                    height='58'
+                    viewBox="0 0 59 58"
+                    fill="none"
+                    alt='noalt'
+                    className='logo'
+                />
+
+                <div id="milieu">
+                    <ul id='central'>
+                        <li>
+                            <button
+                                className={activeButton === 'trajet' ? "active" : ""}
+                                onClick={() => handleClick('trajet')}
+                            >
+                                Trajet
+                            </button>
                         </li>
                         <li>
-                        <a href="#trois" className="trois">
-                            Compte</a>
+                            <button
+                                className={activeButton === 'partager' ? "active" : ""}
+                                onClick={() => handleClick('partager')}
+                            >
+                                Partager
+                            </button>
                         </li>
-                    
-                </ul>
-            </div>
-            
-            <div className="navbar">
-                <div className="dropdown">
-                    <button className="dropbtn">
-                        <img src={ajout} 
-                        id='logo-15'
-                        width='39'
-                        height='39'
-                        viewBox="0 0 39 39"
-                        fill="none"
-                        alt='noalt'/>
-                        <i className="fa fa-caret-down test" />
-                    </button>
-                    <div className="dropdown-content">
-                        <a href="#">Connexion <i className="fa fa-caret-right test2" /></a>
-                        <a href="#">Inscription <i className="fa fa-caret-right test2" /></a>
-                        <div className="esthe">
-                            <a href="#">Trajet <i className="fa fa-caret-right test3" /></a>
-                            <a href="#">Compte <i className="fa fa-caret-right  test4" /></a>
-                            <a href="#">Partager <i className="fa fa-caret-right test5" /></a>
+                        <li>
+                            <button
+                                className={activeButton === 'compte' ? "active" : ""}
+                                onClick={() => handleClick('compte')}
+                            >
+                                Compte
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="navbar">
+                    <div className="dropdown">
+                        <button
+                            className={activeButton === 'connexion' ? "dropbtn active" : "dropbtn"}
+                            onClick={() => handleClick('connexion')}
+                        >
+                            <img
+                                src={ajout}
+                                id='logo-15'
+                                width='39'
+                                height='39'
+                                viewBox="0 0 39 39"
+                                fill="none"
+                                alt='noalt'
+                            />
+                            <i className="fa fa-caret-down test" />
+                        </button>
+                        <div className="dropdown-content">
+                            <button onClick={() => handleClick('connexion')}>
+                                Connexion <i className="fa fa-caret-right test2" />
+                            </button>
+                            <button onClick={() => handleClick('inscription')}>
+                                Inscription <i className="fa fa-caret-right test2" />
+                            </button>
+                            <div className="esthe">
+                                <button
+                                    className={activeButton === 'trajet' ? "active" : ""}
+                                    onClick={() => handleClick('trajet')}
+                                >
+                                        Trajet <i className="fa fa-caret-right test3" />
+                                </button>
+                                <button
+                                    className={activeButton === 'compte' ? "active" : ""}
+                                    onClick={() => handleClick('compte')}
+                                >
+                                    Compte <i className="fa fa-caret-right test4" />
+                                </button>
+                                <button
+                                    className={activeButton === 'partager' ? "active" : ""}
+                                    onClick={() => handleClick('partager')}
+                                >
+                                    Partager <i className="fa fa-caret-right test5" />
+                                </button>
+                            </div>
                         </div>
-                        
                     </div>
                 </div>
-            </div>
-           
-    </nav>
-    </div>
-  )
+            </nav>
+        </div>
+    );
 }
-}
+
 export default Navbar;
