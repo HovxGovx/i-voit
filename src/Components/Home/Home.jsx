8f25b296c7f6cbd533acc495cdff9489ebf7e13a
HovxGovx <angelotrandria69@gmail.com>
2024-04-27 19:00:00 +0300

    exemple sans style

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,41 +1,47 @@
 import './home.css'
 import logo from '../Assets/Icons/covoiturage.png';
+import Exemple from './exemple';
 const Home = () => {
     return (
         <>
             <section className="home" id="home">
                 <div className="home-text">
                     <h1>
-                    Covoiturez, libérez les voies,  <br />  <span>i-Voit !</span>
+                        Covoiturez, libérez les voies,  <br />  <span>i-Voit !</span>
                     </h1>
                     <p> Votre plateforme de covoiturage simple, rapide et conviviale pour des trajets quotidiens ou des escapades. </p>
-                
+
                 </div>
                 <div className="home-img">
-                    <img src={logo} alt="Logo i-voit"/>
+                    <img src={logo} alt="Logo i-voit" />
                 </div>
             </section>
 
             <section className="about" id="about">
                 <div className="about">
-                    <img src={logo} alt="Logo i-voit"/>
+                    <img src={logo} alt="Logo i-voit" />
                 </div>
                 <div className="about-text">
                     <h2>Le covoiturage , c'est quoi ?</h2>
-                    <p>Le covoiturage, également connu sous le nom de covoiturage automobile, 
+                    <p>Le covoiturage, également connu sous le nom de covoiturage automobile,
                         est une pratique où plusieurs personnes se partagent un même véhicule pour se rendre à un endroit commun.
-                         Cette méthode de transport permet aux individus de diviser les frais liés au voyage, tels que l'essence, 
-                         ce qui rend le déplacement plus économique pour tous les participants.</p>
+                        Cette méthode de transport permet aux individus de diviser les frais liés au voyage, tels que l'essence,
+                        ce qui rend le déplacement plus économique pour tous les participants.</p>
                     <p>Les plateformes de covoiturage en ligne facilitent la mise en relation entre conducteurs et passagers en leur permettant de publier et de rechercher des trajets correspondant à leurs besoins et à leurs préférences.
-                         Ces plateformes offrent souvent des fonctionnalités telles que la réservation en ligne, 
-                         le partage des frais de manière équitable et la vérification des profils des utilisateurs pour garantir la sécurité et la confiance.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quisquam voluptatem recusandae, neque at
+                        Ces plateformes offrent souvent des fonctionnalités telles que la réservation en ligne,
+                        le partage des frais de manière équitable et la vérification des profils des utilisateurs pour garantir la sécurité et la confiance.</p>
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quisquam voluptatem recusandae, neque at
                         deleniti consectetur aliquid nisi. Pariatur harum rem iste consequatur? Ipsam amet earum enim tempore
-                        aliquam? Ex.</p>
+                        aliquam? Ex.</p> */}
                     <button className="btn">Learn more</button>
                 </div>
             </section>
-            
+            <section className='products'>
+                <div class="heading">
+                    <h2>Our popular products</h2>
+                </div>
+                <Exemple />
+            </section>
         </>
     );
 }
--- src/Components/Home/exemple.jsx
+++ src/Components/Home/exemple.jsx
@@ -1,8 +1,43 @@
+import './home.css'
 const Exemple = () => {
-    return ( 
-    <>
-    
-    </> );
+    return (
+        <>
+            <div classname="products-container">
+                <div className="box">
+                    <img src="./" />
+                    <h3>Aericano Pure</h3>
+                    <div className="content">
+                        <span>$25</span>
+                        <button className="btn">Add to cart</button>
+                    </div>
+                </div>
+                <div className="box">
+                    <img src="img/partage-de-voiture.png" />
+                    <h3>Aericano Pure</h3>
+                    <div className="content">
+                        <span>$25</span>
+                        <button className="btn">Add to cart</button>
+                    </div>
+                </div>
+                <div className="box">
+                    <img src="img/partage-de-voiture(1).png" />
+                    <h3>Aericano Pure</h3>
+                    <div className="content">
+                        <span>$25</span>
+                        <button className="btn">Add to cart</button>
+                    </div>
+                </div>
+                <div className="box">
+                    <img src="img/partage-de-voiture(1).png" />
+                    <h3>Aericano Pure</h3>
+                    <div className="content">
+                        <span>$25</span>
+                        <button className="btn">Add to cart</button>
+                    </div>
+                </div>
+            </div>
+
+        </>);
 }
- 
+
 export default Exemple;
