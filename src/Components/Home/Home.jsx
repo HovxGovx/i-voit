857934f8a86fddc820a0bdb4ef5d75b3f8a12a98
HovxGovx <angelotrandria69@gmail.com>
2024-04-28 15:41:49 +0300

    minor

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import './home.css'
 import logo from '../Assets/Icons/covoiturage.png';
-import Exemple from './exemple';
+import special from '../Assets/Icons/partage-de-voiture.png'
+import special2 from '../Assets/Icons/partage-de-voiture(1).png'
 const Home = () => {
     return (
         <>
@@ -30,18 +31,69 @@ const Home = () => {
                     <p>Les plateformes de covoiturage en ligne facilitent la mise en relation entre conducteurs et passagers en leur permettant de publier et de rechercher des trajets correspondant à leurs besoins et à leurs préférences.
                         Ces plateformes offrent souvent des fonctionnalités telles que la réservation en ligne,
                         le partage des frais de manière équitable et la vérification des profils des utilisateurs pour garantir la sécurité et la confiance.</p>
-                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quisquam voluptatem recusandae, neque at
-                        deleniti consectetur aliquid nisi. Pariatur harum rem iste consequatur? Ipsam amet earum enim tempore
-                        aliquam? Ex.</p> */}
                     <button className="btn">Learn more</button>
                 </div>
             </section>
-            <section className='products'>
-                <div class="heading">
-                    <h2>Our popular products</h2>
+            {/* Products */}
+            <section className="products" id="products">
+                <div className="heading">
+                    <h2>Les trajets disponibles</h2>
                 </div>
-                <Exemple />
+                {/* container */}
+                <div className="products-container">
+                    <div className="box">
+                        <h3>Origin : <span> Point de depart</span></h3>
+                        <h3>Destination:<span>Point d'arrive</span></h3>
+                        <h3>Heure:<span>12:34</span></h3>
+                        <h3>Place disponible:<span>4</span></h3>
+                        <div className="content">
+                            <span>$25</span>
+                            <button className="btn">Reserver</button>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={special}/>
+                        <h3>Aericano Pure</h3>
+                        <div className="content">
+                            <span>$25</span>
+                            <button className="btn">Add to cart</button>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={special2} />
+                        <h3>Aericano Pure</h3>
+                        <div className="content">
+                            <span>$25</span>
+                            <button className="btn">Add to cart</button>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={special} />
+                        <h3>Aericano Pure</h3>
+                        <div className="content">
+                            <span>$25</span>
+                            <button className="btn">Add to cart</button>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={special2} />
+                        <h3>Aericano Pure</h3>
+                        <div className="content">
+                            <span>$25</span>
+                            <button className="btn">Add to cart</button>
+                        </div>
+                    </div>
+                    <div className="box">
+                        <img src={special}/>
+                        <h3>Aericano Pure</h3>
+                        <div className="content">
+                            <span>$25</span>
+                            <button className="btn">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
             </section>
+
         </>
     );
 }
